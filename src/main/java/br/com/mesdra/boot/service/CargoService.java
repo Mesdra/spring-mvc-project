8cd5057b15d578ceab0d728616c94158bd215108
Mesdra <vinicavalari97@gmail.com>
2020-02-10 09:34:41 -0300

    first commit

---
+++ src/main/java/br/com/mesdra/boot/service/CargoService.java
@@ -0,0 +1,20 @@
+package br.com.mesdra.boot.service;
+
+import java.util.List;
+
+import br.com.mesdra.boot.domain.Cargo;
+
+public interface CargoService {
+
+	void salvar(Cargo cargo);
+	
+	void editar(Cargo cargo);
+	
+	void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+	
+	boolean cargoTemFuncionarios(Long id);
+}
